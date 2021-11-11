d9714016bc4b8743396e7163cd39b8e921be3661
Neil Hyde <neil.hyde17@gmail.com>
2021-11-11 14:43:41 -0600

    fixed faultyItem list change issue

--- scriptHelper.js
+++ scriptHelper.js
@@ -65,8 +65,10 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     if (fuelLevel >= 10000 && cargoLevel <= 10000) {
 
         list.style.visibility = 'visible';
-        launchStatus.innerHTML = 'Shuttle is ready for launch';
+        launchStatus.innerHTML = 'Shuttle is ready for Launch';
         launchStatus.style.color = 'rgb(65, 159, 106)';
+        fuelStatus.innerHTML = 'Fuel level high enough for launch';
+        cargoStatus.innerHTML = 'Cargo mass low enough for launch';
     };
     
 };
